b7329ea3a9b46b7bba3af6c2ab7131818aebcb48
Ryan Han <ryan.han@uwaterloo.ca>
2023-11-05 13:07:11 -0500

    update

--- run_prediction.py
+++ run_prediction.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
 import openai_api
 from src import utils, dataset_loader
 import os
@@ -8,19 +5,10 @@ import openai
 
 
 def query_openai(context_list, setting_name, n_multiply=3):
-    n = len(openai_api.API_name_key_list) * n_multiply
-    try:
-        print("multi-thread n =", n)
-        if setting_name == 'complete':
-            results = openai_api.multi_threading_running(
-                openai_api.query_azure_openai_complete, context_list, n=n)
-        else:
-            results = openai_api.multi_threading_running(
-                openai_api.query_azure_openai_chat, context_list, n=n)
-    except openai.error.APIConnectionError as e:
-        print("found error:", e)
-        return query_openai(context_list, setting_name, max(max(n_multiply-5, n_multiply//2), 1))
-    return results
+    if setting_name == "complete":
+        return [openai_api.query_openai_complete(context) for context in context_list]
+    else:
+        return [openai_api.query_openai_chat(context) for context in context_list]
 
 
 def query_openai_with_retry(context_list, setting_name, retry_time=4, results=None):
@@ -48,8 +36,11 @@ def query_openai_with_retry(context_list, setting_name, retry_time=4, results=No
         for item in filtered_results:
             if utils.extract_answer(item) != "":
                 retry_succeeded += 1
-        print("In the retry, {0} samples succeeded, {1} samples failed".format(
-            retry_succeeded, len(filtered_results) - retry_succeeded))
+        print(
+            "In the retry, {0} samples succeeded, {1} samples failed".format(
+                retry_succeeded, len(filtered_results) - retry_succeeded
+            )
+        )
         if retry_succeeded <= 3:
             retry_time -= 1
     assert len(results) == len(context_list)
@@ -62,18 +53,20 @@ def run_multiple_dataset_batch(work_items):
     print("work items:", work_items)
     dataset_list = []
     item_list = []
-    for (input_path, output_path, mode, _) in work_items:
+    for input_path, output_path, mode, _ in work_items:
         assert mode == work_items[0][2]
         js_list = utils.read_jsonl(input_path)
         content_list = [item["context"] for item in js_list]
         dataset_list.append(len(content_list))
         item_list += content_list
 
-    results = query_openai_with_retry(context_list=item_list, setting_name=work_items[0][2])
+    results = query_openai_with_retry(
+        context_list=item_list, setting_name=work_items[0][2]
+    )
 
     s = 0
     for i in range(len(dataset_list)):
-        utils.save_jsonl(results[s:s + dataset_list[i]], work_items[i][1])
+        utils.save_jsonl(results[s : s + dataset_list[i]], work_items[i][1])
         s += dataset_list[i]
     assert s == len(results)
 
@@ -82,7 +75,7 @@ def run_multiple_dataset(work_items):
     batch = []
     count = 0
     batch_size = 1000
-    if openai_api.default_engine == 'gpt-4':
+    if openai_api.default_engine == "gpt-4":
         batch_size = 500
     for item in work_items:
         if os.path.exists(item[1]):
@@ -119,18 +112,23 @@ if __name__ == "__main__":
         "gaokao-mathqa",
         "gaokao-english",
         "sat-math",
-        "sat-en", "aqua-rat",
-        "lsat-ar", "lsat-lr", "lsat-rc",
-        "logiqa-en", "logiqa-zh",
+        "sat-en",
+        "aqua-rat",
+        "lsat-ar",
+        "lsat-lr",
+        "lsat-rc",
+        "logiqa-en",
+        "logiqa-zh",
         "gaokao-mathcloze",
-        "jec-qa-kd", "jec-qa-ca",
+        "jec-qa-kd",
+        "jec-qa-ca",
         "math",
         "sat-en-without-passage",
     ]
     setting_name_list = [
         # 'few-shot', 'few-shot-CoT',
-        'zero-shot',
-        'zero-shot-CoT'
+        "zero-shot",
+        "zero-shot-CoT",
     ]
     skip_stage_1 = False
     skip_stage_2 = True
@@ -141,52 +139,95 @@ if __name__ == "__main__":
     for dataset_name in dataset_name_list:
         for setting_name in setting_name_list:
             dataset = dataset_loader.load_dataset(
-                dataset_name, setting_name, dataset_dir,
-                prompt_path=raw_prompt_path, max_tokens=2048,
-                end_of_example="<END>\n", verbose=True, chat_mode=chat_mode)
+                dataset_name,
+                setting_name,
+                dataset_dir,
+                prompt_path=raw_prompt_path,
+                max_tokens=2048,
+                end_of_example="<END>\n",
+                verbose=True,
+                chat_mode=chat_mode,
+            )
             # dataset = dataset[:10]
-            input_path = os.path.join(output_dir, "inputs", f"{dataset_name}.{setting_name}.jsonl")
+            input_path = os.path.join(
+                output_dir, "inputs", f"{dataset_name}.{setting_name}.jsonl"
+            )
             utils.save_jsonl(dataset, input_path)
             # dataset = dataset[:10]
             # print(dataset[0]['context'])
             output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl')
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl",
+            )
             first_stage_output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.first_stage.jsonl')
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.first_stage.jsonl",
+            )
 
-            if 'few-shot' in setting_name:
-                work_items.append((input_path, output_path, 'chat' if chat_mode else 'complete', len(dataset)))
+            if "few-shot" in setting_name:
+                work_items.append(
+                    (
+                        input_path,
+                        output_path,
+                        "chat" if chat_mode else "complete",
+                        len(dataset),
+                    )
+                )
             else:
-                work_items.append((input_path, first_stage_output_path, 'chat', len(dataset)))
+                work_items.append(
+                    (input_path, first_stage_output_path, "chat", len(dataset))
+                )
 
     if not skip_stage_1:
-        run_multiple_dataset([item for item in work_items if item[2] == 'complete'])
-        run_multiple_dataset([item for item in work_items if item[2] == 'chat'])
+        run_multiple_dataset([item for item in work_items if item[2] == "complete"])
+        run_multiple_dataset([item for item in work_items if item[2] == "chat"])
 
     work_items = []
     for dataset_name in dataset_name_list:
         for setting_name in setting_name_list:
-            if 'few-shot' in setting_name:
+            if "few-shot" in setting_name:
                 continue
             dataset = dataset_loader.load_dataset(
-                dataset_name, setting_name, dataset_dir,
-                prompt_path=raw_prompt_path, max_tokens=2048,
-                end_of_example="<END>\n", verbose=True)
+                dataset_name,
+                setting_name,
+                dataset_dir,
+                prompt_path=raw_prompt_path,
+                max_tokens=2048,
+                end_of_example="<END>\n",
+                verbose=True,
+            )
             # dataset = dataset[:10]
-            input_path = os.path.join(output_dir, "inputs", f"{dataset_name}.{setting_name}.jsonl")
+            input_path = os.path.join(
+                output_dir, "inputs", f"{dataset_name}.{setting_name}.jsonl"
+            )
             # dataset = dataset[:10]
             # print(dataset[0]['context'])
             output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl')
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl",
+            )
             first_stage_output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.first_stage.jsonl')
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.first_stage.jsonl",
+            )
 
             first_stage_results = utils.read_jsonl(first_stage_output_path)
             second_stage_input = dataset_loader.generate_second_stage_input(
-                dataset_name, dataset, first_stage_results)
-            second_stage_input_path = os.path.join(output_dir, "inputs", f"{dataset_name}.{setting_name}.second_stage.jsonl")
+                dataset_name, dataset, first_stage_results
+            )
+            second_stage_input_path = os.path.join(
+                output_dir,
+                "inputs",
+                f"{dataset_name}.{setting_name}.second_stage.jsonl",
+            )
             utils.save_jsonl(second_stage_input, second_stage_input_path)
-            work_items.append((second_stage_input_path, output_path, 'chat', len(dataset)))
+            work_items.append(
+                (second_stage_input_path, output_path, "chat", len(dataset))
+            )
     if not skip_stage_2:
         openai_api.default_engine = "chatgpt"
         run_multiple_dataset(work_items)
@@ -201,11 +242,21 @@ if __name__ == "__main__":
         ]
         for dataset_name, setting_name in wrong_dataset_name_setting_name_list:
             zero_shot_dataset = dataset_loader.load_dataset(
-                dataset_name, "zero-shot", dataset_dir,
-                prompt_path=raw_prompt_path, max_tokens=2048,
-                end_of_example="<END>\n", verbose=True)
+                dataset_name,
+                "zero-shot",
+                dataset_dir,
+                prompt_path=raw_prompt_path,
+                max_tokens=2048,
+                end_of_example="<END>\n",
+                verbose=True,
+            )
             few_shot_output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl')
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.jsonl",
+            )
             few_shot_second_stage_output_path = os.path.join(
-                output_dir, "outputs", f'predict.{gpt_model}.{dataset_name}.{setting_name}.second_stage.jsonl')
-            
+                output_dir,
+                "outputs",
+                f"predict.{gpt_model}.{dataset_name}.{setting_name}.second_stage.jsonl",
+            )
